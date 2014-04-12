edc38da276d30ef8524f5593fb84ff0f973d6b8b
jamarin6 <jamarinhidalgo@gmail.com>
2014-04-12 17:07:31 +0000

    generating a Comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410105202) do
+ActiveRecord::Schema.define(version: 20140412162101) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -20,11 +20,14 @@ ActiveRecord::Schema.define(version: 20140410105202) do
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "text"
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
