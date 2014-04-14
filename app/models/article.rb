17c126bc4d0c362e926f01360fe3d8de6c635726
jamarin6 <jamarinhidalgo@gmail.com>
2014-04-14 18:31:30 +0000

    Deleting Comments & associated objects

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy     #esto permite que cuando borremos un articulo lo haga tambien sus comentarios
 	validates :title, presence: true,
 					  length: { minimum: 5 }
 end
